9229b689b1deff08d98ed5dc509903d20b61534d
FreddyPoly <frederic@wecraftapps.com>
2018-11-28 22:12:44 +0100

    Style layout enigmes

--- src/pages/Enigmas.js
+++ src/pages/Enigmas.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
 import enigmas from '../data/enigmas.json';
+import fond from '../assets/fond.jpg';
 
 import * as components from './enigmas/index';
 
@@ -21,15 +22,27 @@ class Enigmas extends Component {
 
     return (
       <div className="Enigmas">
-        <header>
-          <p>
-            Enigmas
-          </p>
+        <header style={{ display: 'flex', justifyContent: 'center'}}>
+          <h2 style={{ fontSize: 64, color: 'white'}}>
+            { currentEnigma.name }
+          </h2>
         </header>
 
-        <Link to='/'>Go To Home</Link>
+        <Link style={{position: 'absolute', top: 40, left: 20}} to='/'>Go To Home</Link>
 
         <$ComponentEnigma />
+
+        <div style={{
+          width: '100%',
+          height: '100%',
+          backgroundImage: `url(${fond})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          opacity: .65,
+          position: 'absolute',
+          top: 0,
+          zIndex: -1,
+        }}></div>
       </div>
     );
   }
